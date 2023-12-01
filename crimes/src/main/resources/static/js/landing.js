e61be23aa231ddb615a72d53b5ec5fec30bd1de3
uhnguyen101 <uhnguyen101@gmail.com>
2023-12-01 14:34:22 -0800

    updated contact form - still not working doe :(

--- crimes/src/main/resources/static/js/landing.js
+++ crimes/src/main/resources/static/js/landing.js
@@ -1,1 +1,13 @@
-alert(hi);
+let form = document.getElementById("form");
+function openForm() {
+    form.classList.add("open-form");
+}
+function closeForm() {
+    form.classList.remove("open-form");
+
+}
+window.onclick = function(event) {
+    if (event.target == form) {
+        form.classList.remove("open-form");
+    }
+}
